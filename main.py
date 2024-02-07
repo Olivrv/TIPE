d770d43c317bd233ca9c177160ee9273a25021b9
Olivier <60824785+Olivrv@users.noreply.github.com>
2024-02-07 20:15:18 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import imageio.v3 as im3
+from matplotlib.pyplot import imshow, show
+from PIL import Image
+
+BLACK = 30
+BACKGROUND_COLOR = (255, 255, 255)
+
+
+# noinspection SpellCheckingInspection
+def creer_image_vide_de_taille(img):
+    empty = Image.new('RGB', (img.shape[1], img.shape[0]), (0, 0, 0, 0))
+    empty.save('images/empty.png')
+    return None
+
+
+# noinspection SpellCheckingInspection
+def luminosite(pixel):
+    r, g, b = pixel
+    lum = 0.2126 * r + 0.7152 * g + 0.0722 * b  # ITU-R BT.709
+    return lum
+
+
+def nettoyage_image(img):
+    """On élimine les contours de l'image qui sont sombres (ie arrière-plan),
+    on les remplace par la couleur BACKGROUND_COLOR."""
+    h, l, _ = img.shape
+    empty = im3.imread("images/empty.png")
+    for i in range(h):
+        for j in range(l):
+            if luminosite(img[i][j]) > BLACK:
+                empty[i][j] = img[i][j]
+            else:
+                empty[i][j] = BACKGROUND_COLOR
+    return empty
+
+
+def main():
+    img = im3.imread('images/image.png')
+    imshow(img)
+    show()
+    imshow(nettoyage_image(img))
+    show()
+
+
+main()
---
+++ todo.md
@@ -0,0 +1,11 @@
+## Objectifs
+BUT : suivre l'évolution à partir d'une photo et/ou d'une vidéo de la tension d'un fluide savonneux.
+### Fonctions annexes :
+- traitement d'image : décomposition de l'image et identification des coins.
+  - Resources : 
+    - [Imageio](https://imageio.readthedocs.io/en/stable/)
+    - [Pillow](https://pillow.readthedocs.io/en/stable/)
+### Rendu : 
+On veut l'évolution de l'épaisseur, donc à une image, on renvoie la même avec en noir les 
+points d'épaisseur maximale (relative) et sur un gradient de gris les points du fluide, 
+jusqu'à ceux d'épaisseur nulle en blanc.
