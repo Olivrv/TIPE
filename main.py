50f1e98b4c363c6d31249a16434898640ad13957
Olivier <60824785+Olivrv@users.noreply.github.com>
2024-05-24 17:46:01 +0200

    GO

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ BLACK = 30
 BACKGROUND_COLOR = (255, 255, 255)
 
 
-# noinspection SpellCheckingInspection
 def creer_image_vide_de_taille(img):
     empty = Image.new('RGB', (img.shape[1], img.shape[0]), (0, 0, 0, 0))
     empty.save('images/empty.png')
     return None
 
+
+def is_black(pixel):
+    # TODO
+    pass
 
 # noinspection SpellCheckingInspection
 def luminosite(pixel):
@@ -37,6 +40,37 @@ def nettoyage_image(img):
 def extraire_couleurs(img):
     pass
     # TODO
+
+
+def destruction_noir(image_list : list, n) -> list:
+    """On reconnait les pixels noirs sur les n premières images du film,
+    qu'on remplace par du blanc sur toutes les images consécutives (nettoyage du noir
+    périphérique, présent avant éclatement du film)"""
+    noirs = set()
+    for image in range(n):
+        img = image_list[image]
+        h, l, _ = img.shape
+        for i in range(h):
+            for j in range(l):
+                if is_black(img[i][j]):
+                    noirs.add((i, j))
+    for image in range(len(image_list)):
+        for (i, j) in noirs:
+            image_list[image][i][j] = BACKGROUND_COLOR
+    return image_list
+
+
+def first_black(images_list: list) -> int:
+    """Dans une liste d'image de type im3, renvoie le premier indice où apparait une lame sombre"""
+    li = list(images_list)
+    for i in range(len(li)):
+        image = li[i]
+        h, l, _ = image.shape
+        for k in range(h):
+            for j in range(l):
+                if luminosite(image[k][j]) < BLACK:
+                    return i
+    return -1
 
 
 def main():
--- previous.py
+++ previous.py
@@ -2,23 +2,25 @@ import numpy as np
 import scipy.integrate as si
 import matplotlib.pyplot as plt
 
-Eclairement = lambda sigma, d: 1 - np.cos(2 * np.pi * d * sigma * 10 ** (-9))
+
+def Eclairement(sigma, d):
+    return 1 - np.cos(2 * np.pi * d * sigma * (10 ** (-9)))
 
 
 def Eclairementbis(d):
-    return si.quad(Eclairement, 1 / (4 * 10 ** (-7)), 1 / (7.5 * 10 ** (-7)), args=(d,))
+    return si.quad(Eclairement, 1 / (4 * (10 ** (-7))), 1 / (7.5 * (10 ** (-7))), args=(d,))
 
 
 def A1(d):
-    return si.quad(Eclairement, 2.07 * 10 ** 6, 2.31 * 10 ** 6, args=(d,))
+    return si.quad(Eclairement, 2.07 * (10 ** 6), 2.31 * (10 ** 6), args=(d,))
 
 
 def A2(d):
-    return si.quad(Eclairement, 1.71 * 10 ** 6, 1.95 * 10 ** 6, args=(d,))
+    return si.quad(Eclairement, 1.71 * (10 ** 6), 1.95 * (10 ** 6), args=(d,))
 
 
 def A3(d):
-    return si.quad(Eclairement, 1.3 * 10 ** 6, 1.54 * 10 ** 6, args=(d,))
+    return si.quad(Eclairement, 1.3 * (10 ** 6), 1.54 * (10 ** 6), args=(d,))
 
 
 def B1(d):
@@ -37,11 +39,11 @@ a, b = 1000, 300
 D1, D2, D3 = [], [], []
 for i in range(1, 2 * b + 2):
     D1.append(Eclairementbis(-a * (-1.00001 + (i - 1) / b))[0] \
-              / 2 / ((4 * 10 ** (-7)) - 1 / (7.5 * 10 ** (-7))) * B1(-a * (-1.00001 + (i - 1) / b)))
+              / 2 / ((4 * (10 ** (-7))) - 1 / (7.5 * (10 ** (-7)))) * B1(-a * (-1.00001 + (i - 1) / b)))
     D2.append(Eclairementbis(-a * (-1.00001 + (i - 1) / b))[0] \
-              / 2 / ((4 * 10 ** (-7)) - 1 / (7.5 * 10 ** (-7))) * B2(-a * (-1.00001 + (i - 1) / b)))
+              / 2 / ((4 * (10 ** (-7))) - 1 / (7.5 * (10 ** (-7)))) * B2(-a * (-1.00001 + (i - 1) / b)))
     D3.append(Eclairementbis(-a * (-1.00001 + (i - 1) / b))[0] \
-              / 2 / ((4 * 10 ** (-7)) - 1 / (7.5 * 10 ** (-7))) * B3(-a * (-1.00001 + (i - 1) / b)))
+              / 2 / ((4 * (10 ** (-7))) - 1 / (7.5 * (10 ** (-7)))) * B3(-a * (-1.00001 + (i - 1) / b)))
     plt.plot([(-a)*(-1+(i-1))/b, 0], [(-a)*(-1+(i-1))/b, 1], color=(D3[i-1], D2[i-1], D3[i-1]))
     plt.title("Echelle des teintes de Newton")
     plt.ylabel("Différence de marche (nm)")
